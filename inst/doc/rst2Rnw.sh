3ffc75789ce9b91d9a3963cbbe88ac7eb60263fa
Matt Shotwell <biostatmatt@gmail.com>
2011-07-28 16:30:00 -0500

    add script to convert rst -> Rnw

---
+++ inst/doc/rst2Rnw.sh
@@ -0,0 +1,6 @@
+rst2latex sas7bdat.rst > sas7bdat.tex
+sed -re '/^%%% User specified packages and stylesheets/a \
+    \\usepackage{fullpage}\
+    \\usepackage{Sweave}\
+    \\VignetteIndexEntry{sas7bdat}' sas7bdat.tex > sas7bdat.Rnw
+rm sas7bdat.tex
