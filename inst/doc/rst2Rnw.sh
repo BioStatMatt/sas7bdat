e3952408be432b245e0053b9724f49826417f3c5
Matt Shotwell <biostatmatt@gmail.com>
2011-08-17 08:38:22 -0500

    fixed \VignetteIndexEntry handling

--- inst/doc/rst2Rnw.sh
+++ inst/doc/rst2Rnw.sh
@@ -2,5 +2,5 @@ rst2latex sas7bdat.rst > sas7bdat.tex
 sed -re '/^%%% User specified packages and stylesheets/a \
     \\usepackage{fullpage}\
     \\usepackage{Sweave}\
-    \\VignetteIndexEntry{sas7bdat}' sas7bdat.tex > sas7bdat.Rnw
+    %\\VignetteIndexEntry{sas7bdat}' sas7bdat.tex > sas7bdat.Rnw
 rm sas7bdat.tex
